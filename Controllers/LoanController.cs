8e56bb16397b25185c78f52852864c61675a6306
robertmorris <ryanmorris793@gmail.com>
2016-01-17 11:51:22 -0500

    First commit

---
+++ Controllers/LoanController.cs
@@ -0,0 +1,59 @@
+﻿using MyExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyExample.Controllers
+{
+    public class LoanController : Controller
+    {
+        // GET: Loan
+        public ActionResult Index()
+        {
+            var loans = new List<Loan> {
+                new Loan {
+                    PurchasePrice = 247000,
+                    LoanTerm = 30,
+                    InterestRate = 4.0,
+                    DownPayment = 20000,
+                    PropertyTax = 2000,
+                    PMI = 100,
+                    MonthlyPayment = 0
+                },
+                new Loan {
+                    PurchasePrice = 247000,
+                    LoanTerm = 30,
+                    InterestRate = 4.0,
+                    DownPayment = 20000,
+                    PropertyTax = 2000,
+                    PMI = 100,
+                    MonthlyPayment = 0
+                }
+            };
+            return View(loans);
+        }
+
+        List<Loan> loans = new List<Loan> {
+                new Loan {
+                    PurchasePrice = 247000,
+                    LoanTerm = 30,
+                    InterestRate = 4.0,
+                    DownPayment = 20000,
+                    PropertyTax = 2000,
+                    PMI = 100,
+                    MonthlyPayment = 0
+                },
+                new Loan {
+                    PurchasePrice = 247000,
+                    LoanTerm = 30,
+                    InterestRate = 4.0,
+                    DownPayment = 20000,
+                    PropertyTax = 2000,
+                    PMI = 100,
+                    MonthlyPayment = 0
+                }
+            };
+    }
+}
---
+++ Models/Loan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyExample.Models
+{
+    public class Loan
+    {
+        public double PurchasePrice { get; set; }
+        public double LoanTerm { get; set; }
+        public double InterestRate { get; set; }
+        public double DownPayment { get; set; }
+        public double PropertyTax { get; set; }
+        public double PMI { get; set; }
+        public double MonthlyPayment { get; set; }
+    }
+}
